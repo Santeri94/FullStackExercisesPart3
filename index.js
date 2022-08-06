f1b9fe7d8052b901e2564cc712442b44bb2c6515
Santeri Pulkkinen <santeri.pulkkinen@centria.fi>
2022-08-06 14:52:16 +0300

    deleted hard-coded phonebook-list

--- index.js
+++ index.js
@@ -9,30 +9,6 @@ app.use(cors())
 app.use(express.static('build'))
 const Person = require('./models/person')
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-
 app.get('/info', (request, response) => {
     const entries = persons.length
     const date = new Date ()
