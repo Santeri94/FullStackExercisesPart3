6b9e036eeb94c22dbd43cbafb23e6566cab5cc4d
Santeri Pulkkinen <santeri.pulkkinen@centria.fi>
2022-08-09 20:31:36 +0300

    adding eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,17 +12,7 @@ module.exports = {
         'indent': [
             'error',
             2
-            
         ],
-        'eqeqeq': 'error',
-        'no-trailing-spaces': 'error',
-        'object-curly-spacing': [
-            'error', 'always'
-        ],
-        'arrow-spacing': [
-            'error', { 'before': true, 'after': true }
-        ],
-        'no-console': 0,
         'linebreak-style': [
             'error',
             'unix'
@@ -34,6 +24,15 @@ module.exports = {
         'semi': [
             'error',
             'never'
-        ]
+        ],
+        'eqeqeq': 'error',
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': [
+            'error', 'always'
+        ],
+        'arrow-spacing': [
+            'error', { 'before': true, 'after': true }
+        ],
+        'no-console': 0,
     }
 }
